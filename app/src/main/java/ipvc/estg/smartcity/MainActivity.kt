a8e46db52ae1cccd1b36652a2acfc95a4e6c22ac
pedcastro19 <pedcastro19@gmail.com>
2021-03-31 17:54:14 +0100

    Commit anterior ao Refactor para AndroidX

--- app/src/main/java/ipvc/estg/smartcity/MainActivity.kt
+++ app/src/main/java/ipvc/estg/smartcity/MainActivity.kt
@@ -1,14 +1,23 @@
 package ipvc.estg.smartcity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        try {
+            this.supportActionBar!!.hide()
+            }
+        catch (e: NullPointerException) {}
         setContentView(R.layout.activity_main)
 
         /*Commit Test*/
         /*New Stuff*/
+
+
+
     }
 }
