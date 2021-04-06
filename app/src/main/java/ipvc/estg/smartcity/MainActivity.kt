2c29d71207bbe0936705a1fb53375b124eef544d
pedcastro19 <pedcastro19@gmail.com>
2021-04-06 01:09:45 +0100

    Layout inicial feito, introdução do layout ''Notepad'', inserção de novos icons

--- app/src/main/java/ipvc/estg/smartcity/MainActivity.kt
+++ app/src/main/java/ipvc/estg/smartcity/MainActivity.kt
@@ -2,6 +2,8 @@ package ipvc.estg.smartcity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,14 @@ class MainActivity : AppCompatActivity() {
             }
         catch (e: NullPointerException) {}
         setContentView(R.layout.activity_main)
+
+
+        // Get input text
+       // val inputText = outlinedTextField.editText?.text.toString()
+
+        //outlinedTextField.editText?.doOnTextChanged { inputText, _, _, _ ->
+            // Respond to input text change
+        //}
 
         /*Commit Test*/
         /*New Stuff*/
---
+++ app/src/main/java/ipvc/estg/smartcity/Notepad.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.smartcity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notepad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.notepad)
+    }
+}
